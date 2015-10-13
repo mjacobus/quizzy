27c64a6a53c5f802e842d2fe4f595924254569df
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-10-13 11:54:21 +0200

    Created migration for quizzes

---
+++ db/migrations/20151013094254_create_quiz.rb
@@ -0,0 +1,12 @@
+Lotus::Model.migration do
+  change do
+    create_table :quizzes do
+      primary_key :id
+      column :title, String, null: false
+      column :description, :text,  null: false
+      column :private, :boolean,  default: true, null: false
+      column :created_at, :datetime
+      column :updated_at, :datetime
+    end
+  end
+end
