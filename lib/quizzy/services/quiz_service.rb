b891185462f324cd43e5e0942288f7b5ac59d2a3
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-10-14 23:06:34 +0200

    Created service layer

---
+++ lib/quizzy/services/quiz_service.rb
@@ -0,0 +1,21 @@
+module Quizzy
+  module Services
+    class QuizService
+      def initialize(repository)
+        @repository = repository
+      end
+
+      def all
+        repository.find_all_by({})
+      end
+
+      def create(service)
+        repository.create(service)
+      end
+
+      private
+
+      attr_reader :repository
+    end
+  end
+end
